cb67965d948996009648e07aeac670f04db422fd
Miroslav Hasák <miro.hasak@gmail.com>
2023-12-06 15:19:40 +0100

    dlzka funguje s malou odchylkou niekedy inf

--- QrCodeLengthReader.py
+++ QrCodeLengthReader.py
@@ -1,6 +1,7 @@
 import cv2
 from pyzbar.pyzbar import decode
 import numpy as np
+import pickle
 
 # Inicializace kamery
 cap = cv2.VideoCapture(0)
@@ -9,11 +10,19 @@ cap = cv2.VideoCapture(0)
 start_scanning = False
 
 # Skutečné rozměry QR kódu (v centimetrech)
-qr_code_width_cm = 16.5  # Upravte na skutečné rozměry QR kódu
+qr_code_width_cm = 18.7  # Upravte na skutečné rozměry QR kódu
 
 # Rozlišení obrazu kamery
 camera_width = 2688
 camera_height = 1520
+
+# Načítanie kalibračných parametrov
+try:
+    with open("calibration.pkl", "rb") as f:
+        camera_matrix, distortion_coefficients = pickle.load(f)
+except FileNotFoundError:
+    print("Súbor s kalibráciou nebol nájdený. Skontrolujte, či ste vykonali kalibráciu.")
+    exit()
 
 while True:
     ret, frame = cap.read()
@@ -33,8 +42,9 @@ while True:
                 # Získejte šířku QR kódu v pixelech
                 qr_width_px = abs(points[0][0] - points[1][0])
 
-                # Vypočítejte vzdálenost od kamery k QR kódu (přibližně)
-                distance = (qr_code_width_cm * camera_width) / (2 * qr_width_px)
+                # Vypočítejte vzdálenost od kamery k QR kódu s kalibračnými parametrami
+                undistorted_points = cv2.undistortPoints(np.array([[[points[0][0], points[0][1]]]], dtype=np.float32), camera_matrix, distortion_coefficients)
+                distance = (qr_code_width_cm * camera_matrix[0, 0]) / qr_width_px
 
                 print(f'Vzdálenost od kamery k QR kódu: {distance:.2f} cm')
 
