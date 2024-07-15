7aeec155446c984890084b13abb1b6eb7d7f5b74
Miroslav Hasák <miro.hasak@gmail.com>
2024-07-15 13:29:13 +0200

    finalne

--- QrCodeLengthReader.py
+++ QrCodeLengthReader.py
@@ -40,18 +40,27 @@ while True:
 
             distance = (qr_code_width_cm * camera_matrix[0, 0]) / qr_width_px
 
-            horizontal_angle = np.degrees(np.arctan2((points[0][0] + points[1][0]) / 2 - camera_width / 2, camera_matrix[0, 0]))
-            vertical_angle = np.degrees(np.arctan2((points[0][1] + points[3][1]) / 2 - camera_height / 2, camera_matrix[1, 1]))
+            # Výpočet stredných hodnôt bodov pre horizontálny a vertikálny uhol
+            mid_x_h = (points[0][0] + points[1][0]) / 2
+            mid_y_v = (points[0][1] + points[3][1]) / 2
+
+            horizontal_angle = np.degrees(np.arctan2(mid_x_h - camera_width / 2, camera_matrix[0, 0]))
+            vertical_angle = np.degrees(np.arctan2(mid_y_v - camera_height / 2, camera_matrix[1, 1]))
 
             print(f'Vzdialenost: {2 * distance:.2f} cm')
             print(f'Horizontalny uhol: {47 + horizontal_angle:.2f} stupnov')
             print(f'Vertikalny uhol: {50 + vertical_angle:.2f} stupnov')
 
             cv2.putText(frame, f'Vzdialenost: {2 * distance:.0f} cm', (points[0][0], points[0][1] + 120), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-
             cv2.putText(frame, f'Horizontalny uhol: {47 + horizontal_angle:.2f} stupnov', (points[0][0], points[0][1] + 160), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-
             cv2.putText(frame, f'Vertikalny uhol: {50 + vertical_angle:.2f} stupnov', (points[0][0], points[0][1] + 180), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+
+            # Vyznačenie všetkých 4 rohov na obrázku
+            #cv2.circle(frame, (points[0][0], points[0][1]), 5, (255, 0, 0), -1) # Bod 0 - Modrý
+            #cv2.circle(frame, (points[1][0], points[1][1]), 5, (0, 255, 0), -1) # Bod 1 - Zelený
+            #cv2.circle(frame, (points[2][0], points[2][1]), 5, (0, 255, 255), -1) # Bod 2 - Žltý
+            #cv2.circle(frame, (points[3][0], points[3][1]), 5, (0, 0, 255), -1) # Bod 3 - Červený
+            #cv2.circle(frame, (int(mid_x_h), int(mid_y_v)), 5, (255, 255, 0), -1) # Stredná hodnota - Svetložltý
 
     cv2.imshow("QR Code Scanner", frame)  
 
